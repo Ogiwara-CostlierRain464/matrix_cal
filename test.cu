84dac2701da79979611dd01686dde6ccb02ef716
yushiogiwara <yushiogiwara@gmail.com>
2024-06-11 12:03:36 +0900

    support binary matmul in cuda

--- test.cu
+++ test.cu
@@ -9,34 +9,38 @@
 #include <array>
 #include <cassert>
 
-__global__ void tcMatMul(const half* const a_ptr,
-                       const half* const b_ptr,
-                       half* const c_ptr){
-    nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, 16, 16, 16, half, nvcuda::wmma::col_major> a_frag;
-    nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, 16, 16, 16, half, nvcuda::wmma::col_major> b_frag;
-    nvcuda::wmma::fragment<nvcuda::wmma::accumulator, 16, 16, 16, half> c_frag;
+__global__ void tcMatMul(const signed char* const a,
+                       const signed char* const b,
+                       int* const c){
+    nvcuda::wmma::fragment<nvcuda::wmma::matrix_a, 16, 16, 16, signed char, nvcuda::wmma::col_major> a_frag;
+    nvcuda::wmma::fragment<nvcuda::wmma::matrix_b, 16, 16, 16, signed char, nvcuda::wmma::col_major> b_frag;
+    nvcuda::wmma::fragment<nvcuda::wmma::accumulator, 16, 16, 16, int> c_frag;
 
-
-    nvcuda::wmma::fill_fragment(c_frag, __float2half(.0f));
-
-    nvcuda::wmma::load_matrix_sync(a_frag, a_ptr, 16);
-    nvcuda::wmma::load_matrix_sync(b_frag, b_ptr, 16);
+    nvcuda::wmma::load_matrix_sync(a_frag, a, 16);
+    nvcuda::wmma::load_matrix_sync(b_frag, b, 16);
+    nvcuda::wmma::fill_fragment(c_frag, 0);
 
     nvcuda::wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
 
-    nvcuda::wmma::store_matrix_sync(c_ptr, c_frag, 16, nvcuda::wmma::mem_col_major);
+    nvcuda::wmma::store_matrix_sync(c, c_frag, 16, nvcuda::wmma::mem_col_major);
 }
 
-__global__ void cuMatMul(const half* const a_ptr,
-                       const half* const b_ptr,
-                       half* const c_ptr){
-    // cの一行目はbの1行…となるように計算
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void cuMatMul(const char* const a,
+                       const char* const b,
+                       int* const c){
+    // assert elements in a must be {-1, 0, 1}
 
-    for(size_t i = 0; i < 16; i++){
-        c_ptr[i * 16 + idx] = b_ptr[i * 16 + idx];
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    for(size_t row = 0; row < 16; row++){
+        int accum = 0;
+        for(size_t i = 0; i < 16; i++){
+            if(a[row * 16 + i] == 1) {
+                accum += b[i * 16 + col];
+            }
+        }
+        c[row * 16 + col] = accum;
     }
-
 }
 
 float measureKernel(std::function<void(void)> fn){
@@ -60,7 +64,19 @@ float measureKernel(std::function<void(void)> fn){
     return milliseconds;
 }
 
-void make_I(std::array<half, 256> &b){
+void make_binary(std::array<char, 256> &a){
+    a.fill(0);
+
+    // 1+1が右端と左端にある
+    for(size_t r = 0; r < 16; r++){
+        a[0 + r * 16] = 1;
+        a[1 + r * 16] = 1;
+        a[14 + r * 16] = 1;
+        a[15 + r * 16] = 1;
+    }
+}
+
+void make_I(std::array<char, 256> &b){
     b.fill(0);
     for(size_t i = 0; i < 16; i++){
         b.at(i + i * 16) = 1;
@@ -68,43 +84,37 @@ void make_I(std::array<half, 256> &b){
 }
 
 int main(int argc, char** argv){
-    half *a;
-    cudaMalloc((void**)  &a, sizeof(half) * 16 * 16 );
-    std::array<half, 256> a_ar;
-    a_ar.fill(1);
+    char *a; cudaMalloc((void**)  &a, sizeof(char) * 16 * 16 );
+    std::array<char, 256> a_ar; make_binary(a_ar);
+    cudaMemcpy(a, a_ar.data(), 256 * sizeof(char), cudaMemcpyHostToDevice);
 
-    half *b;
-    cudaMalloc((void**)  &b, sizeof(half) * 16 * 16 );
-    std::array<half, 256> b_ar;
-    make_I(b_ar);
+    char *b; cudaMalloc((void**)  &b, sizeof(char) * 16 * 16 );
+    std::array<char, 256> b_ar; make_I(b_ar);
+    cudaMemcpy(b, b_ar.data(), 256 * sizeof(char), cudaMemcpyHostToDevice);
 
-    half *c;
-    cudaMalloc((void**)  &c, sizeof(half) * 16 * 16 );
-    cudaMemset(c, 0, sizeof(half) * 16 * 16);
-
-    std::array<half, 256> c_ar;
-
-    cudaMemcpy(a, a_ar.data(), 256 * sizeof(half), cudaMemcpyHostToDevice);
-    cudaMemcpy(b, b_ar.data(), 256 * sizeof(half), cudaMemcpyHostToDevice);
+    int *c; cudaMalloc((void**)  &c, sizeof(int) * 16 * 16 ); cudaMemset(c, 0, sizeof(int) * 16 * 16);
+    std::array<int, 256> c_ar;
 
     float ms = measureKernel([a, b, c](){
         // 32でないとだめ
-        tcMatMul<<<1, 32>>>(a, b, c);
+        for(size_t i = 0; i < 1000; i++){
+            tcMatMul<<<1, 32>>>(( signed char * ) a, ( signed char * )  b, c);
+        }
     });
     std::cout << "TensorCore Time: " << ms << "ms" << std::endl;
 
-    cudaMemcpy(c_ar.data(), c, 256 * sizeof(half), cudaMemcpyDeviceToHost);
-    assert(c_ar.at(34) == half(1) && "what");
-
-    c_ar.fill(0); // clear
+    cudaMemcpy(c_ar.data(), c, 256 * sizeof(int), cudaMemcpyDeviceToHost);
+    assert(c_ar.at(0) == 1 && "what");
 
     ms = measureKernel([a, b, c](){
-        cuMatMul<<<1, 16>>>(a, b, c);
+        for(size_t i = 0; i < 1000; i++){
+            cuMatMul<<<1, 16>>>( a, b, c);
+        }
     });
     std::cout << "CudaCore Time: " << ms << "ms" << std::endl;
-    cudaMemcpy(c_ar.data(), c, 256 * sizeof(half), cudaMemcpyDeviceToHost);
-    assert(c_ar.at(0) == half(1) && "what");
-    assert(c_ar.at(17) == half(1) && "what");
+    cudaMemcpy(c_ar.data(), c, 256 * sizeof(int), cudaMemcpyDeviceToHost);
+    assert(c_ar.at(0) == 1 && "what");
+    assert(c_ar.at(17) == 1 && "what");
 
     return 0;
 }
